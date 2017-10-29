cf2ac6a478d1b74da880ef2a808242953c305144
nick <nzozor@gmail.com>
2017-10-29 14:53:16 +0000

    particules

--- js/creative.js
+++ js/creative.js
@@ -23,7 +23,7 @@
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#mainNav',
-        offset: 48
+        offset: 500
     });
 
     // Collapse the navbar when page is scrolled
